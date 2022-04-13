b9b27750cc30205c5340f481176ce1fad5b9b2b1
Dylan Nguyen <dylannguyen@macbookpro.socal.rr.com>
2022-04-13 14:12:41 -0700

    created initial variables

---
+++ main.py
@@ -0,0 +1,24 @@
+from re import search
+import speech_recognition as sr
+import pyttsx3 
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import pyautogui
+import os
+import subprocess 
+import sys
+from bs4 import BeautifulSoup
+import requests
+from keyboard import press
+from keyboard import press_and_release
+import webbrowser
+
+
+listener = sr.Recognizer()
+engine = pyttsx3.init('sapi5')
+rate = engine.getProperty("rate")
+engine.setProperty("rate", 180)
+
+
