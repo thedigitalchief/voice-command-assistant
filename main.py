56876ac2c26c6ad44184b69538568c62f6fcd739
Dylan Nguyen <93560178+thedigitalchief@users.noreply.github.com>
2022-04-13 13:42:17 -0700

    created initial file, added imports

---
+++ main.py
@@ -0,0 +1,16 @@
+from re import search
+import speech_recognition as sr
+import pyttsx3 
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import pyautogui
+import os
+import subprocess 
+import sys
+from bs4 import BeautifulSoup
+import requests
+from keyboard import press
+from keyboard import press_and_release
+import webbrowser
