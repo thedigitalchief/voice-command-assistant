e48119a314354b7118596bdb0761770222b87cd9
HorvathTeddy <72626608+HorvathTeddy@users.noreply.github.com>
2022-04-24 18:53:33 -0700

    Added comment

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ from keyboard import press
 from keyboard import press_and_release
 import webbrowser
 
-
+# initial variable setup with engine and listener
 listener = sr.Recognizer()
 engine = pyttsx3.init('sapi5')
 rate = engine.getProperty("rate")
 engine.setProperty("rate", 180)
 
+# talk() function that takes text from the user
 def talk(text):
     engine.say(text)
     engine.runAndWait()
