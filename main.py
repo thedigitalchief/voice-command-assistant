dfc240a5b43c80de666c1028400fa06fbd353302
thedigitalchief <me@dylanhnguyen.com>
2022-04-25 17:35:52 -0700

    updating master branch

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ import webbrowser
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-<<<<<<< HEAD
-=======
 
 # initializing variables along with engine and listener
 listener = sr.Recognizer()
@@ -36,9 +34,6 @@ def talk(text):
 
 # outputs voice prompt "Hello sir"
 talk('Hello sir')
-<<<<<<< HEAD
->>>>>>> e343feddfed297c977940a22350c169c520e02c9
-=======
 
 
 # takes user input via voice 
@@ -58,7 +53,6 @@ def take_command():
             pass   
 
     return command
-
 
 
 def run_computer():
@@ -103,4 +97,4 @@ run_computer()
 # deacticate
 
 # Python 3.10: universal2 for Mac M1 devies
->>>>>>> f8b93cf1d3f26cbf8283748724a467fa704bb566
+
